2e1273f50068fc1ddbcba9e4b9e3139e8cb384d6
Dave Banwell <dave@davebanwell.com>
2025-08-08 17:51:16 -0400

    base user model added and working

--- server/models.py
+++ server/models.py
@@ -3,4 +3,29 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+# user model
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+    
+    # TODO: serializer rules
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    user_type = db.Column(db.String)
+    f_name = db.Column(db.String)
+    l_name = db.Column(db.String)
+    phone_num = db.Column(db.Integer)
+    password_hash = db.Column(db.String)
+    
+    #TODO: add relationships
+    #TODO: add assoc. proxies
+    #TODO: add validation
+    
+    def __repr__(self):
+        return f"<< USER: {self.username} ({self.user_type}) >>"
+
+if __name__ == "__main__":
+    dave = User(id=1, username="banwelld", user_type="customer", f_name="Dave", l_name="Banwell", phone_num="5195668231", password_hash="Thi$isMypa$$w0rd")
+    print(dave, "\n")
+    print(dave.id, dave.user_type, dave.username, dave.f_name, dave.l_name, dave.phone_num, dave.password_hash)
