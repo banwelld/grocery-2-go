3a0aacd5cf08934e4c0cb792bf442b4faeeb5842
Dave Banwell <dave@davebanwell.com>
2025-08-08 17:56:10 -0400

    user db validation added

--- server/models.py
+++ server/models.py
@@ -24,6 +24,28 @@ class User(db.Model, SerializerMixin):
     
     def __repr__(self):
         return f"<< USER: {self.username} ({self.user_type}) >>"
+    
+# item model
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+    
+    # TODO: serializer rules
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    user_type = db.Column(db.String)
+    f_name = db.Column(db.String, nullable=False)
+    l_name = db.Column(db.String, nullable=False)
+    phone_num = db.Column(db.Integer, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    
+    #TODO: add relationships
+    #TODO: add assoc. proxies
+    #TODO: add validation
+    
+    def __repr__(self):
+        return f"<< USER: {self.username} ({self.user_type}) >>"
 
 if __name__ == "__main__":
     dave = User(id=1, username="banwelld", user_type="customer", f_name="Dave", l_name="Banwell", phone_num="5195668231", password_hash="Thi$isMypa$$w0rd")
