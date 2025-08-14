e974172f284a3a0b0e6c7a1d07ab5eeaf61ae7b2
Dave Banwell <dave@davebanwell.com>
2025-08-13 22:02:21 -0400

    AllItems get functionality works

--- server/app.py
+++ server/app.py
@@ -18,5 +18,14 @@ def index():
     return '<h1>Project Server</h1>'
 
 
+class AllItems(Resource):
+    
+    def get(self):
+        items = [i.to_dict(rules=("-order_items",)) for i in Item.query.all()]
+        return make_response(items, 200)
+    
+api.add_resource(AllItems, "/items")
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/config.py
+++ server/config.py
@@ -1,21 +1,27 @@
-# library imports
 
+import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from dotenv import load_dotenv
+
+# open env files
+
+load_dotenv(dotenv_path="settings.env")
 
 # Instantiate app, set attributes
 
-DATABASE = "postgresql://Mosaic@localhost:5432/Mosaic"
+DB_URL = os.getenv("DB_URL")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 # Define metadata, instantiate db
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
